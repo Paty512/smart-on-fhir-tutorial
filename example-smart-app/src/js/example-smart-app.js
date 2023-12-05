af01ca090e2cf9f73fd16695ee33ce0b76bca535
Paty512 <152983721+Paty512@users.noreply.github.com>
2023-12-05 13:00:38 -0600

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -24,8 +24,7 @@ var dia_loinc ="8462-4";
                           'http://loinc.org|2089-1', //LDL
                           'http://loinc.org|+bp_loinc', //blood pressure
                           'http://loinc.org|8310-5'] //body temp
-                    },date:'gt1980-01-01'
-                  });
+                    },date:'gt1980-01-01'};
 
         $.when(pt, obv).fail(onError);
 
